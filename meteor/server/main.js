69f686889fd063eea56a041c39c9b704883cfe9b
nimec01 <24428341+nimec01@users.noreply.github.com>
2025-03-25 16:11:24 +0100

    remove remaining database code

--- meteor/server/main.js
+++ meteor/server/main.js
@@ -4,9 +4,7 @@ import './methods/validate'
 import './methods/getInstances'
 import './methods/nextInstances'
 import './methods/getProjection'
-import './methods/getModel'
 
-import './publications/modelFromLink'
 
 
 Meteor.startup(() => {
