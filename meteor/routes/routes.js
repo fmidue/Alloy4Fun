053fd9abf1ddd3bf89ffeb215a80f7c1adbc50ae
nimec01 <24428341+nimec01@users.noreply.github.com>
2025-12-03 08:48:36 +0000

    remove blaze-layout

--- meteor/routes/routes.js
+++ meteor/routes/routes.js
@@ -2,17 +2,17 @@
  * Defines the routes for the application
  */
 import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
-import { BlazeLayout } from 'meteor/kadira:blaze-layout';
+import { Blaze } from "meteor/blaze";
 
 FlowRouter.route('/', {
   name: 'alloyEditor',
   action() {
-    BlazeLayout.render('mainLayout', { content: 'alloyEditor' });
+    Blaze.render(Template.alloyEditor, document.body);
   }
 });
 
 FlowRouter.route('*', {
   action() {
-    BlazeLayout.render('mainLayout', { content: 'notFound' });
+    Blaze.render(Template.notFound, document.body);
   }
 });
