6062dac8db12b25f03593a136c8e399c93866fd8
nimec01 <24428341+nimec01@users.noreply.github.com>
2025-12-03 08:12:43 +0000

    replace blaze-html-templates

--- meteor/client/main.js
+++ meteor/client/main.js
@@ -1,5 +1,18 @@
 import { Meteor } from 'meteor/meteor'
 
+import '../imports/ui/layouts/mainLayout.html';
+import './templates/alloyEditor/alloyEditor.html';
+import './templates/alloyEditor/alloyEditor';
+import './templates/loading/loading.html';
+import './templates/notFound/notFound.html';
+import './templates/textEditor/textEditor.html';
+import './templates/textEditor/textEditor';
+import './templates/visSettings/frameNavigation/frameNavigation.html';
+import './templates/visSettings/frameNavigation/frameNavigation';
+import './templates/visSettings/rightClickMenu.html';
+import './templates/visSettings/rightClickMenu';
+
+
 Meteor.startup(() => {
     // code to run on client at startup
 
--- meteor/routes/routes.js
+++ meteor/routes/routes.js
@@ -3,7 +3,6 @@
  */
 import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
-import '../imports/ui/layouts/mainLayout.html';
 
 FlowRouter.route('/', {
   name: 'alloyEditor',
